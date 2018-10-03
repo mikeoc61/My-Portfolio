2418cd1810b088f35ebf9be9ee7a592ebfd4bbdb
Michael OConnor <gmikeoc@gmail.com>
2018-10-03 10:27:59 -0500

    minor changes to sample portfolio

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ Note: need to run "npm run webpack" for any changes to be visible
 
 const myWork = [
   {
-    'title': "Dynamic Serverless Portfolio",
+    'title': "Serverless Portfolio",
     'href': "https://github.com/mikeoc61/My-Portfolio",
     'desc': "Dynamic Portfolio project written in Javascript and utilizing Github, SSH, HTML, CSS, Font Awesome, Google Fonts, React, NPM, webpack, babel and jest.  When changes are pushed to github the code is picked up by AWS CodePipeline and built using AWS CodeBuild. Lambda puts the finished product into an S3 bucket which serves as the HTML root for my portfolio website (portfolio.mikeoc.me). AWS SNS notifies me when the build happens and if the deploy is successful.",
     'image': {
@@ -17,17 +17,6 @@ const myWork = [
       'comment': ""
     }
   },
-  {
-    'title': "GetMacs Python Project",
-    'href': "https://github.com/mikeoc61/GetMacs",
-    'desc': "Simple Python function to scan a /24 address range and report MAC addresses for valid IPs.",
-    'image': {
-      'desc': "GetMacs Python Project",
-      'src': "images/example_1.jpg",
-      'comment': ""
-    }
-  },
-
   {
     'title': "AWS Button / IFTTT",
     'href': "https://github.com/mikeoc61/IFTTT-Stuff",
@@ -38,8 +27,8 @@ const myWork = [
       'comment': ""
     }
   },
-    {
-      'title': "URL Monitor",
+  {
+    'title': "URL Monitor",
       'href': "https://github.com/mikeoc61/Web_Site_Monitor",
       'desc': "Monitors a specific URL for availability, latency and for change by computing and comparing a SHA1 hash. If configured, program also uses AWS boto3 to provide alerts via SNS and prompts use for AWS Profile and Mobile #",
       'image': {
@@ -47,6 +36,16 @@ const myWork = [
         'src': "images/AWS_logo.jpg",
         'comment': ""
       }
+  },
+    {
+    'title': "GetMacs",
+    'href': "https://github.com/mikeoc61/GetMacs",
+    'desc': "Simple Python function to scan a /24 address range and report corresponding MAC addresses for valid IPs.",
+    'image': {
+      'desc': "GetMacs Python Project",
+      'src': "images/example_1.jpg",
+      'comment': ""
+    }
   }
 ]
 
