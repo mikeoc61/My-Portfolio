8f614e2d5d5d60101a0adb20ae00fc7589001ae1
Michael OConnor <gmikeoc@gmail.com>
2018-12-31 10:41:44 -0600

    Updated Currency Monitor description

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ const myWork = [
   {
     'title': "Currency Monitor",
     'href': "https://github.com/mikeoc61/currency_monitor",
-    'desc': "Monitors basket of foreign currencies for changes relative to USD and displays color coded changes on console. Utilizes DyanmoDB as a persistent datastore so that current rates can be compared with saved rates and change can be calculaed. Also utilizes S3 and CloudFront CDN to store static CSS and Header HTML.",
+    'desc': "Python programs implemented as command line and AWS Lambda functions which monitor a basket of foreign currencies for changes relative to USD. Utilizes AWS DynamoDB as a persistent datastore so that current rates can be compared with saved rates and change percentage can be calculated. Utilizes AWS S3 Storage and CloudFront CDN for CSS and Header HTML content.",
     'image': {
       'desc': "A Serverless Portfolio",
       'src': "images/currency.jpg",
