af708591153a203938abc785ee7b705ae1aecd3f
Michael OConnor <gmikeoc@gmail.com>
2019-01-04 10:34:23 -0600

    Testing out line continuation for desc

--- js/main.js
+++ js/main.js
@@ -10,7 +10,14 @@ const myWork = [
   {
     'title': "Serverless Portfolio",
     'href': "https://github.com/mikeoc61/My-Portfolio",
-    'desc': "Dynamic Portfolio project written in Javascript and utilizing Github, SSH, HTML, CSS, Font Awesome, Google Fonts, React, NPM, webpack, babel and jest.  When changes are pushed to github the code is picked up by AWS CodePipeline and built using AWS CodeBuild. Lambda puts the finished product into an S3 bucket which serves as the HTML root for my portfolio website (portfolio.mikeoc.me). AWS SNS notifies me when the build happens and if the deploy is successful.",
+    'desc': "Dynamic Portfolio project written in Javascript and \
+             utilizing Github, SSH, HTML, CSS, Font Awesome, Google Fonts, \
+             React, NPM, webpack, babel and jest.  When changes are pushed to \
+             github the code is picked up by AWS CodePipeline and built using \
+             AWS CodeBuild. Lambda puts the finished product into an S3 bucket \
+             which serves as the HTML root for my portfolio website \
+             (portfolio.mikeoc.me). AWS SNS notifies me when the build happens \
+             and if the deploy is successful.",
     'image': {
       'desc': "A Serverless Portfolio",
       'src': "images/example_2.jpg",
